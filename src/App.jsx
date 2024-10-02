c41e803e99765ca554094ad0d33d149e14ffa9d1
dasushan <53913896+dasushan@users.noreply.github.com>
2024-10-02 16:36:36 +0530

    creating navbar component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Navbar } from './components/shared/Navbar';
+
+function App() {
+  return (
+    <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
