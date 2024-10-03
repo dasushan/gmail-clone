e7af550399bdfe0ed64f9a57c49a075dbed7ae59
dasushan <53913896+dasushan@users.noreply.github.com>
2024-10-03 10:13:22 +0530

    adding react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/shared/Navbar';
 import Sidebar from './components/Sidebar';
+import Inbox from './components/Inbox';
+import Mail from './components/Mail';
+import Body from './components/Body';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />,
+    children: [
+      {
+        path: '/',
+        element: <Inbox />,
+      },
+      {
+        path: '/mail/:id',
+        element: <Mail />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
+    <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <Navbar />
-      <Sidebar />
+      <RouterProvider router={router} />
     </div>
   );
 }
